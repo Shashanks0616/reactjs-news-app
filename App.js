36ad7f271be78607b8d045e5631594b889d65369
Shashank Singh <80109899+Shashanks0616@users.noreply.github.com>
2021-08-29 20:13:09 +0530

    This  is the news app made by using react js.

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './component/Navbar';
+import News from './component/News';
+import NewsItem from './component/NewsItem';
+
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <News/>
+   {/* <NewsItem/> */}
+   </>
+  );
+}
+
+export default App;
